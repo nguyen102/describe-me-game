8aa09ad15a410e15313478ddee8600cb7b119735
Joo Lee <joo@videoblocks.com>
2016-04-08 14:12:16 -0400

    ui frames

--- client/main.js
+++ client/main.js
@@ -2,21 +2,16 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../imports/ui/game.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Meteor.startup(function () {
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+    if (!Meteor.isClient) {
+        return;
+    }
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_ONLY'
+    });
+
 });
