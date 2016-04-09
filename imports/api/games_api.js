1d4ed109ef47f80c489d4416a5c15790a4f9bb6d
Joo Lee <joo@videoblocks.com>
2016-04-09 00:35:51 -0400

    mostly

--- imports/api/games_api.js
+++ imports/api/games_api.js
@@ -1,56 +1,75 @@
 import { Meteor } from 'meteor/meteor';
 
 Games = new Meteor.Collection("games");
-Scores = new Meteor.Collection("scores");
-WordHistory = new Meteor.Collection("word_history");
-var imageUrls = ["https://d1yn1kh78jj1rr.cloudfront.net/preview/cal-retrotoons-0814-192_M.jpg",
-"http://cdn1.theodysseyonline.com/files/2015/12/21/6358631429926013411708851658_Dog-Pictures.jpg", 
+
+var imageUrls = [
+    "https://d1yn1kh78jj1rr.cloudfront.net/preview/cal-retrotoons-0814-192_M.jpg",
+    "http://cdn1.theodysseyonline.com/files/2015/12/21/6358631429926013411708851658_Dog-Pictures.jpg",
     "http://media.caranddriver.com/images/media/51/2016-10best-cars-lead-photo-664005-s-original.jpg",
     "http://weknowyourdreamz.com/images/island/island-09.jpg",
     "https://images.trvl-media.com/media/content/expus/graphics/launch/cruise1320x742.jpg",
     "http://az616578.vo.msecnd.net/files/2016/03/05/635928088052093326-1019743780_woman_smelling_flowers_in_the_field.jpg"
-]
+];
+
 Meteor.startup(() => {
 
     if (Meteor.isServer) {
         
-        _createGame = function (playerId) {
-            let game = Games.insert({
-                gameId: 1,
-                player1: null,
-                player2: null,
-                started: false,
-                timeLeft: 60,
-                startTime: null,
-                score: 0,
-                imageUrl: "https://d1yn1kh78jj1rr.cloudfront.net/preview/cal-retrotoons-0814-192_M.jpg"
+        function _joinGame(playerId) {
+
+            let availableGame = Games.findOne({
+                player1: {$exists: true},
+                player2: {$exists: false},
+                started: false
             });
-            return game;
-        };
-        Games.remove({});
-        Scores.remove({});
-        WordHistory.remove({});
-        
-        _createGame(1);
-        Scores.insert({name:"game1", score: 0});
+
+            if(availableGame){
+                availableGame.player2 = playerId;
+                availableGame.started = true;
+                availableGame.timeLeft = 120;
+                availableGame.totalScore = 0;
+                availableGame.imageUrl = Random.choice(imageUrls);
+                availableGame.done = false;
+
+                Games.update({_id: availableGame._id}, availableGame);
+
+                Meteor.setTimeout(function() {
+                    _resetImage(availableGame);
+                }, 1000);
+
+                return availableGame._id;
+
+            }else{
+
+                let gameId = Games.insert({
+                    player1: playerId,
+                    started: false
+                });
+
+                return gameId;
+            }
+        }
+
+        function _resetImage(game){
+
+            console.log(game._id + "yo");
+            game.timeLeft = game.timeLeft - 15;
+            game.imageUrl = imageUrls[_.random(0, 5)];
+            if(game.timeLeft > 0){
+                Meteor.setTimeout(function() {
+                    _resetImage(game);
+                }, 15000);
+            }
+        }
 
         Meteor.methods({
-            'updatePicture': function(index) {
-                var gameId = Games.findOne()._id;
-                Games.update({_id: gameId}, 
-                    {$set: {imageUrl: imageUrls[index]}});
+            'joinGame': function(playerId) {
+                return _joinGame(playerId);
             }
         });
 
-        Meteor.publish("games", function() {
-            return Games.find();
+        Meteor.publish("games", function(gameId) {
+            return Games.find({_id: gameId});
         });
-        Meteor.publish("scores", function() {
-            return Scores.find();
-        });
-        Meteor.publish("word_history", function() {
-            return WordHistory.find();
-        });
-
-    };
+    }
 });
--- imports/ui/game.js
+++ imports/ui/game.js
@@ -1,116 +1,24 @@
 import './game.html';
-Games = new Meteor.Collection("games");
-Scores = new Meteor.Collection("scores");
-WordHistory = new Meteor.Collection("word_history");
 
-var lastEnteredWord = "";
-var clockTime = 60;
+Games = new Meteor.Collection("games");
 
 Template.game.onCreated(function gameOnCreated() {
-    Meteor.subscribe("games");
-    Meteor.subscribe("scores");
-    Meteor.subscribe("word_history");
-    timeCountDown = setInterval(function(){
-        var timeLeft = clockTime;
-        if (Games.findOne().startTime == null){
-            id = Games.findOne()._id;
-            date = new Date();
-            time = Math.floor(date.getTime() / 1000);
-            Games.update({_id: id}, {$set: {startTime: time}});
-        }else {
-            startTime = Games.findOne().startTime;
-            date = new Date();
-            time = Math.floor(date.getTime() / 1000);
-            timeLeft = clockTime - (time - startTime);
-            // Games.update({_id: id}, {$set: {startTime: time}});
-        }
-        if(timeLeft % 10 == 0){
-            Meteor.call("updatePicture", 6 - Math.floor(timeLeft / 10), function(error, result){});
-        }
-        // timeLeft = timeLeft - 1;
-        Session.set("time", timeLeft);
-        if (timeLeft <= 0){
-            clearInterval(timeCountDown);
-        }
-    }, 1000);
+
+    Meteor.call('joinGame', Meteor.userId(), function(error, gameId){
+        Meteor.subscribe("games", gameId);
+    });
 });
 
 Template.game.events({
     'click #answer-button': function (e) {
-        _sendWord();
-        _updateScore();
-        _resetAnswerBox();
+        alert('chill for now');
     },
     'keyup #answer-box': function (e) {
         if (e.type == "keyup" && e.which == 13) {
-            _sendWord();
-            _updateScore();
-            _resetAnswerBox();
+            alert('chill for now too');
         }
     }
 });
-
-_updateScore = function() {
-    var currentScore = Scores.findOne({name: "game1"}).score;
-    var id = Scores.findOne({name: "game1"})._id;
-
-    var otherUserName;
-    if ( Meteor.user().username == "tim") {
-        otherUserName = "joo";
-    } else {
-        otherUserName = "tim";
-    }
-
-    otherUser = WordHistory.findOne({name: otherUserName});
-    if (otherUser == null) {
-        WordHistory.insert({name:  otherUserName, words: []});
-    }
-    var otherUserWords = WordHistory.findOne({name: otherUserName}).words;
-    for(var i = 0; i < otherUserWords.length; i++){
-        if(lastEnteredWord == otherUserWords[i]) {
-            _removeWordFromWordHistory(lastEnteredWord, Meteor.user().username);
-            _removeWordFromWordHistory(lastEnteredWord, otherUserName);
-            currentScore += 10;
-        }
-    }
-
-    Scores.update({_id: id}, {$set:{score: currentScore }});
-};
-
-_removeWordFromWordHistory = function(word, userName){
-    var id = WordHistory.findOne({name: userName})._id;
-    var words = WordHistory.findOne({name: userName}).words;
-    var index = words.indexOf(word);
-    if (index > - 1) {
-        words.splice(index, 1);
-    }
-    WordHistory.update({_id: id}, {$set: {words: words}});
-};
-
-
-
-_sendWord = function () {
-    var answerBox = document.getElementById("answer-box");
-    lastEnteredWord = answerBox.value;
-    _addToWordHistory(Meteor.user().username, answerBox.value);
-};
-
-_addToWordHistory = function(username, word) {
-    user = WordHistory.findOne({name: username});
-    if (user == null) {
-        WordHistory.insert({name:  Meteor.user().username, words: []});
-    }
-    var words = WordHistory.findOne({name: Meteor.user().username}).words;
-    if (!_isInArray(word, words)){
-        var id = WordHistory.findOne({name: Meteor.user().username})._id;
-        words.push(word);
-        WordHistory.update({_id: id}, {$set: {words: words}});
-    }
-};
-
-_isInArray = function(value, array) {
-    return array.indexOf(value) > -1;
-};
 
 _resetAnswerBox = function() {
     var answerBox = document.getElementById("answer-box");
@@ -119,22 +27,7 @@ _resetAnswerBox = function() {
 };
 
 Template.game.helpers({
-    sessionId: '',
-    started: true,
-    score: function() {
-        if (Scores.findOne() != null) {
-            return Scores.findOne({name: "game1"}).score;
-        }
-    },
-    timeLeft: function() {
-        return Session.get("time");
-        // if (Games.findOne() != null){
-        //     return Games.findOne().timeLeft;
-        // }
-    },
-    imageUrl: function() {
-        if (Games.findOne() != null){
-            return Games.findOne().imageUrl;
-        }
+    game() {
+        return Games.findOne({});
     }
 });
